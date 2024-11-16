a9c12ebcc1ae0c42a90ba65df7dac79401a3bd78
Jiahui Xu <nemo5754@outlook.com>
2024-11-16 17:08:03 +0000

    feat:网格完成

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-import { Button } from 'antd';
+import { Row, Col, Button } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [row, setRow] = useState(4)
+  const [col, setCol] = useState(3)
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
+    <div className="container">
+      <div className="left" style={{ display: "gird", gridTemplateRows: `repeat(${row}, 1fr)`, gridTemplateColumns: `repeat(${col}, 1fr)` }}>
+        {[...Array(row * col)].map((_, index) => (
+          <div key={index} className="grid-item">
+            {`Item ${index + 1}`}
+          </div>
+        ))}
+      </div>
+      <div className="right">
+        <div className="right-top">
+          <Button type='primary'>Fun1</Button>
+          <Button type='primary'>Fun1</Button>
+          <Button type='primary'>Fun1</Button>
+          <Button type='primary'>Fun1</Button>
+        </div>
+        <div className="right-mid">
+          {/* 右边底部内容 */}
+          右边底部
+        </div>
+        <div className="right-bottom">
+          <Button type='primary'>Start</Button>
+          <Button type='primary'>Clear</Button>
+        </div>
+      </div>
     </div>
   );
 }
